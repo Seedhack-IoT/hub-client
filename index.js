390c6406833c8b124e6fcce8bf9f20b8284ab946
Mansimran Singh <me@mansimransingh.com>
2015-01-25 02:47:21 +0000

    hacky shit:

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ var fs = require('fs');
 var Connection = require('ssh2');
 var keyDir = __dirname+'/keys';
 var sshKey = keyDir + '/foo_rsa';
+    // sshKey = '/Users/mani/.ssh/id_rsa'
 var comment = 'device_name';
 var password = false;
 var haiku = require('./haiku.js');
 var polo = require("polo");
 var apps = polo();
-
+var Channel = require('./node_modules/ssh2/lib/Channel.js');
 var clientData = null;
 var conn = null;
 var hub = null;
+var sys = require('sys');
+var exec = require('child_process').exec;
+var child = null;
+// var exec = require('child_process').exec;
+
 
 var getmacAddress = function(callback){
     require('getmac').getMac(function(err,macAddress){
@@ -95,43 +101,159 @@ var updateClientFile = function(callback){
     });
 }
 var discoverHub = function(callback){
+    var address = {host: '10.100.93.145', port:2200};
+    return callback(address);
+
     apps.get("apf-central-hub", function(address) {
         callback(address);
     });
 }
+
+var childStdout = function(data){
+    //PARSE OUTPUT HERE AND DO SHIT WITH IT
+    consolg.log("STDOUT: "+ data);
+}
+
+var childStdin = function(data){
+    if(child !== null){
+        child.stdin.write(data);
+    }
+}
+
 var makeSshConnection = function(){
-    
+    console.log("Making SSH connection...");
     discoverHub(function(address){        
-        hub = address.host;
-
+        // hub = address.host;
+        console.log("Found our hub...");
         readClient(function(ignoreMe){
+            console.log("Getting ssh key...");
             getSshKey(function(obj){
-                var conn = new Connection();
 
-                conn.on('ready', function() {
-                  console.log('Connection :: ready');
-                  console.log('I should send you client.json now?');
-                  conn._openChan('apf_data',function(err, stream) {
-                    if (err) throw err;
-                    stream.on('exit', function(code, signal) {
-                      console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
-                    }).on('close', function() {
-                      console.log('Stream :: close');
-                      conn.end();
-                    }).on('data', function(data) {
-                      console.log('STDOUT: ' + data);
-                    }).stderr.on('data', function(data) {
-                      console.log('STDERR: ' + data);
+                var ret = {
+                        key: {
+                                priv: sshKey, 
+                                pub: sshKey+'.pub'
+                        },
+                        client: clientData,
+                        hub: address,
+                };
+
+                // console.log(ret);
+
+                fs.writeFile('config.json', JSON.stringify(ret), function(err){
+                    if (err) return err;
+
+                    // So, the ssh client for some reason wont receive anything 
+                    // from the ssh server
+                    // so this is a hacky way to have a ssh client 
+                    // without changing all js code
+                    // client written in go and compiled
+
+                    console.log("Starting Go process");
+
+                    child = exec(__dirname+'/sshClient');
+                    child.stdout.on('data', childStdout);
+
+                    child.stderr.on('data', function(data) {
+                        console.log('stderr: ' + data);
                     });
-                  });
 
-                }).connect({
-                  readyTimeout: 120000,
-                  host: hub,
-                  port: 2200,
-                  username: clientData.uuid,
-                  privateKey: obj.key
+                    child.on('close', function(code) {
+                        console.log('closing code: ' + code);
+                    });
+
                 });
+
+                    
+                
+                
+
+                // var conn = new Connection();
+
+                // console.log("Trying to connect...");
+
+                // conn.on('ready', function() {
+                //   console.log('Connection :: ready');
+
+
+
+
+                //   conn._openChan('apf_data',function(err, chan) { //apf_data
+                //     if (err) throw err;
+
+                //     console.log("Got a channel...");
+                //     console.log("Creating new stream...");
+
+                //     var stream = new Channel.ChannelStream(chan);
+
+                //     stream.on('exit', function(code, signal) {
+                //       console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+                //     });
+                //     // stream.setEncoding('utf8');
+                //     stream.on('close', function() {
+                //       console.log('Stream :: close');
+                //       conn.end();
+                //     });
+
+                //     stream.on('data', function(data) {
+                //       console.log('STDOUT: ' + data);
+                //     });
+
+                //     stream.stderr.on('data', function(data) {
+                //       console.log('STDERR: ' + data);
+                //     });
+
+                //     stream.on('complete' ,function(data){
+                //             console.log(data);
+                //     });
+                //     // console.log(stream._read(10));
+                //     // console.log(chan.incoming);
+                //     // process.stdin.pipe(Channel.pipe
+
+                //     stream.write(JSON.stringify(clientData)+'\n');
+                //   });
+// conn.exec('yes', function(err, stream) {
+//     if (err) throw err;
+//     stream.on('exit', function(code, signal) {
+//       console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+//     }).on('close', function() {
+//       console.log('Stream :: close');
+//       conn.end();
+//     }).on('data', function(data) {
+//       console.log('STDOUT: ' + data);
+//     }).stderr.on('data', function(data) {
+//       console.log('STDERR: ' + data);
+//     });
+//   });
+// conn.shell(function(err, stream) {
+//     if (err) throw err;
+//     stream.on('close', function() {
+//       console.log('Stream :: close');
+//       conn.end();
+//     }).on('data', function(data) {
+//       console.log('STDOUT: ' + data);
+//     }).stderr.on('data', function(data) {
+//       console.log('STDERR: ' + data);
+//     });
+//     stream.write('Hello world!\n');
+//     stream.end('ls -l\nexit\n');
+//   });
+  // conn.sftp(function(err, sftp) {
+  //   if (err) throw err;
+  //   sftp.readdir('/', function(err, list) {
+  //     if (err) throw err;
+  //     console.dir(list);
+  //     conn.end();
+  //   });
+  // });
+
+                // }).connect({
+                //   readyTimeout: 120000,
+                //   host: address.host,
+                //   port: address.port,
+                //   username: clientData.uuid,
+                //   privateKey: obj.key,
+                // });
 
             });
         });
